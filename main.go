412e6b6d5e0cb3265cbb9f3a5a30754bb6a96ce2
amryyahya <amryyahya07@gmail.com>
2024-12-11 23:05:55 +0000

    add user struct

--- main.go
+++ main.go
@@ -1,44 +1,35 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
-    "pomodoro-app/authentication-service/config"
+	"net/http"
+	"pomodoro-app/authentication-service/config"
 	"pomodoro-app/authentication-service/db"
+	"pomodoro-app/authentication-service/models"
+	"github.com/gin-gonic/gin"
 )
 
-type user struct {
-    Email           string  `json:"email"`
-    HashedPassword  string  `json:"hashedPassword"`
-}
-
-var users = []user{
-    {Email: "Blue Train", HashedPassword: "password"},
-    {Email: "Jeru", HashedPassword: "password"},
-    {Email: "Sarah Vaughan and Clifford Brown", HashedPassword: "password"},
-}
-
-func getUsers(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, users)
-}
+// func getUsers(c *gin.Context) {
+//     c.IndentedJSON(http.StatusOK, users)
+// }
 
 func register(c *gin.Context) {
-    var newUser user
+    var newUser models.User
     if err := c.BindJSON(&newUser); err != nil {
         return
     }
-    users = append(users, newUser)
-    c.IndentedJSON(http.StatusCreated, newUser)
+    config.LoadEnv()
+    connStr := config.GetDBConnectionString()
+    database := db.Connect(connStr)
+    models.InsertUser(database,newUser.Email,[]byte{1,2,3},[]byte{4,5})
+	defer database.Close()
+    c.IndentedJSON(http.StatusCreated, "hahahaha")
 }
 
 func main() {
     router := gin.Default()
-    router.GET("/users", getUsers)
+    // router.GET("/users", getUsers)
     router.POST("/register", register)
-    config.LoadEnv()
-    connStr := config.GetDBConnectionString()
-    database := db.Connect(connStr)
-	defer database.Close()
 
-    router.Run("localhost:8080")
+
+    router.Run("localhost:8000")
 }
--- models/user.go
+++ models/user.go
@@ -4,6 +4,11 @@ import (
 	"database/sql"
 )
 
+type User struct {
+    Email           string  `json:"email"`
+    HashedPassword  string  `json:"hashedPassword"`
+}
+
 func InsertUser(db *sql.DB, email string, hashedPassword, salt []byte) error {
 	query := `INSERT INTO users (email, hashed_password, salt) VALUES ($1, $2, $3)`
 	_, err := db.Exec(query, email, hashedPassword, salt)
