95c1a1c8a85e365ef30368dd6fb088c1202c8abe
amryyahya <amryyahya07@gmail.com>
2024-12-12 04:15:45 +0000

    edit user management

--- models/user.go
+++ models/user.go
@@ -17,3 +17,14 @@ func InsertUser(db *sql.DB, email string, hashedPassword, salt []byte) error {
 	}
 	return nil
 }
+
+func GetUserCredByEmail(db *sql.DB, email string) ([]byte, []byte, error) {
+	// get user hashed_password and salt
+	query := `SELECT hashed_password, salt FROM users WHERE email = $1`
+	row := db.QueryRow(query, email)
+    var hashedPassword, salt []byte
+    if err := row.Scan(&hashedPassword, &salt); err != nil {
+        return nil, nil, err
+    }
+    return hashedPassword, salt, nil
+}
