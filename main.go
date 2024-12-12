17baf5d4240a8081aab533c1fa32986f80d3cada
amryyahya <amryyahya07@gmail.com>
2024-12-12 12:33:25 +0000

    add dbpool

--- main.go
+++ main.go
@@ -1,45 +1,79 @@
 package main
 
 import (
+    "database/sql"
 	"fmt"
 	"net/http"
 	"pomodoro-app/authentication-service/config"
 	"pomodoro-app/authentication-service/db"
 	"pomodoro-app/authentication-service/models"
 	"pomodoro-app/authentication-service/utils"
-
 	"github.com/gin-gonic/gin"
 )
 
-// func getUsers(c *gin.Context) {
-//     c.IndentedJSON(http.StatusOK, users)
-// }
+var dbPool *sql.DB
+
+func init(){
+    config.LoadEnv()
+    connStr := config.GetDBConnectionString()
+    dbPool = db.Connect(connStr)
+}
 
 
+func login(c *gin.Context) {
+    var user models.User
+    if err := c.BindJSON(&user); err != nil {
+        return
+    }
+    database := dbPool
+    isUserExisted, _ := models.CheckIfEmailExist(database, user.Email)
+    if !isUserExisted {
+        c.IndentedJSON(http.StatusBadRequest, gin.H{"error":"User not Found"})
+        return
+    }
+    hashedPassword, salt, err := models.GetUserCredByEmail(database, user.Email)
+    if err != nil {
+        fmt.Println(err)
+    }
+    isValid := utils.VerifyPassword(user.Password,hashedPassword,salt)
+    if isValid {
+        c.IndentedJSON(http.StatusCreated, gin.H{"message": "Login Success"})
+    } else {
+        c.IndentedJSON(http.StatusUnauthorized, gin.H{"error": "Wrong Password"})
+    }
+}
 
 func register(c *gin.Context) {
     var newUser models.User
     if err := c.BindJSON(&newUser); err != nil {
+        c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON input"})
         return
     }
-    config.LoadEnv()
-    connStr := config.GetDBConnectionString()
-    database := db.Connect(connStr)
-    hashedPassword, salt, err := utils.HashPassword(newUser.Password)
-	if err != nil {
-		fmt.Printf("Error hashing password: %v\n", err)
-		return
+    database := dbPool
+    isUserExisted, _ := models.CheckIfEmailExist(database, newUser.Email)
+    fmt.Print(isUserExisted)
+	if isUserExisted {
+		c.IndentedJSON(http.StatusConflict, gin.H{"error":"Email Has Been Registered"})
+        return
 	}
-    models.InsertUser(database,newUser.Email,hashedPassword,salt)
-	defer database.Close()
-    c.IndentedJSON(http.StatusCreated, "User has been registered")
+
+    hashedPassword, salt, _ := utils.HashPassword(newUser.Password)
+    err := models.InsertUser(database,newUser.Email,hashedPassword,salt)
+    if err!=nil {
+        println(err)
+        c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "Server Error"})
+        return
+    } 
+    c.IndentedJSON(http.StatusCreated, gin.H{"message" : "Register Success"})
+    
+    
 }
 
 func main() {
     router := gin.Default()
-    // router.GET("/users", getUsers)
-    router.POST("/register", register)
 
+    router.POST("/login", login)
+    router.POST("/register", register)
 
     router.Run("localhost:8000")
 }
