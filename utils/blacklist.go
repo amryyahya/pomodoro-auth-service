0fe9c324868909887a5febbfc39202ea3859955d
amryyahya <amryyahya07@gmail.com>
2024-12-19 04:29:13 +0000

    modify blacklist func

--- utils/blacklist.go
+++ utils/blacklist.go
@@ -2,12 +2,13 @@ package utils
 
 import (
 	"database/sql"
+	"time"
 	_ "github.com/lib/pq" 
 )
 
-func Blacklist_Token(db *sql.DB, user_id string, token string) (string, error) {
+func Blacklist_Token(db *sql.DB, user_id string, token string, expiration *time.Time) error {
 	query := `INSERT INTO token_blacklist (user_id, token, expiry_time) VALUES ($1, $2, $3)`
-	_, err := db.Exec(user_id, token)
+	_, err := db.Exec(query, user_id, token, expiration)
 	if err != nil {
 		return err
 	}
